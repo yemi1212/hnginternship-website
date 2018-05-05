49db5fcc6e8dc27636ddb753d997a30d0f02fe39
Abifarin Joshua <joeshua1212@gmail.com>
2018-05-05 10:25:16 -0500

    website

---
+++ hnginternship.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+    	<link rel="stylesheet" type="text/css" href="style.css">
+    	<title>hnginternship</title>
+    </head>dth: 911px; height: 133px; left: 293px; top: 195px; font-family: sedan; font-styl
+      <body style="position: relative; width: 1440px; height: 1024px; background: #219653; mix-blend-mode: darken;">
+      	<h1 style="position: absolute; wie: normal; font-weight: normal; line-height: normal; font-size: 96px; color: #000;">HNGINTERNSHIP 4</h1>
+      	<div style="position: absolute;width: 506px; height: 133px; left: 483px;top: 401px; font-family: sedan; font-style: normal; font-weight: normal; line-height: normal; font-size: 96px; color: #000;">
+      		<?php date_default_timezone_set('Africa/lagos')
+      		echo date('l, jS F H:i')
+      		?>
+      	</div>
+       </body>
+  </html>
